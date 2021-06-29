e79f76a55c49dee1b0dfa0ee7ccbc0c0327b9770
lxjanan <17104@students.mrgs.school.nz>
2021-06-29 22:15:50 +1200

    1.3 Register Button & Return Button Added

--- app/src/main/java/com/lojanan/managementapp2/Login.java
+++ app/src/main/java/com/lojanan/managementapp2/Login.java
@@ -2,13 +2,36 @@ package com.lojanan.managementapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    // We use private as it cannot be overridden
+    private EditText loginEmail, loginPass;
+    private Button loginButton;
+    private TextView loginRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginEmail = findViewById(R.id.loginE);
+        loginPass = findViewById(R.id.loginP); // This code finds the view for the method
+        loginButton = findViewById(R.id.loginB);
+        loginRegister = findViewById(R.id.loginR);
+
+        loginRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/lojanan/managementapp2/RegisterActivity.java
+++ app/src/main/java/com/lojanan/managementapp2/RegisterActivity.java
@@ -2,13 +2,35 @@ package com.lojanan.managementapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText registerEmail, registerPass;
+    private Button registerButton;
+    private TextView registerReturn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        registerEmail = findViewById(R.id.registerE);
+        registerPass = findViewById(R.id.registerP);
+        registerButton = findViewById(R.id.registerB);
+        registerReturn = findViewById(R.id.registerR);
+
+        registerReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
