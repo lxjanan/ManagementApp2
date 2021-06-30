c1d4413442a1af1f0293e775e2cf1621f408f37c
lxjanan <17104@students.mrgs.school.nz>
2021-06-30 22:52:10 +1200

    1.5 ToDo Activity + AddTask Layout First Version

--- app/src/main/java/com/lojanan/managementapp2/ToDoActivity.java
+++ app/src/main/java/com/lojanan/managementapp2/ToDoActivity.java
@@ -1,14 +1,51 @@
 package com.lojanan.managementapp2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ToDoActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do);
+
+        recyclerView = findViewById(R.id.recyclerV);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setReverseLayout(true);
+        llm.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(llm);
+
+        fab = findViewById(R.id.floatBtn);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTask();
+            }
+        });
+    }
+
+    private void addTask() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        View myView = inflater.inflate(R.layout.addtask_file, null);
+        alertDialog.setView(myView);
+
+        AlertDialog dialog = alertDialog.create();
+        dialog.setCancelable(false);
+        dialog.show();
     }
 }
