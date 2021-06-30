c17b1e0c5aa10dee112f47693bd4b3167410142e
lxjanan <17104@students.mrgs.school.nz>
2021-06-30 23:47:47 +1200

    1.6 Firebase Database Problem Solved + Model Class Added

--- app/src/main/java/com/lojanan/managementapp2/ToDoActivity.java
+++ app/src/main/java/com/lojanan/managementapp2/ToDoActivity.java
@@ -10,16 +10,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ToDoActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private FloatingActionButton fab;
 
+    private DatabaseReference reference;
+    private String userID;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do);
+
+        mAuth = FirebaseAuth.getInstance();
 
         recyclerView = findViewById(R.id.recyclerV);
         LinearLayoutManager llm = new LinearLayoutManager(this);
@@ -27,6 +38,10 @@ public class ToDoActivity extends AppCompatActivity {
         llm.setStackFromEnd(true);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(llm);
+
+        mUser = mAuth.getCurrentUser();
+        userID = mUser.getUid();
+        reference = FirebaseDatabase.getInstance().getReference().child("tasks").child(userID);
 
         fab = findViewById(R.id.floatBtn);
         fab.setOnClickListener(new View.OnClickListener() {
