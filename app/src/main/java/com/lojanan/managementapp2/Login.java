8bd8ca3698f41cf4254a3f56eac78a458da7f270
lxjanan <17104@students.mrgs.school.nz>
2021-07-10 16:09:56 +1200

    2.95 Backup

--- app/src/main/java/com/lojanan/managementapp2/Login.java
+++ app/src/main/java/com/lojanan/managementapp2/Login.java
@@ -88,7 +88,7 @@ public class Login extends AppCompatActivity {
                             finish();
                         },100);
                     }else {
-                        Toast.makeText(Login.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Login.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         progressBar.setVisibility(View.GONE);
                     }
                 }
